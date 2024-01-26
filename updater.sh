f05484ff23eb2ab1d130450f3511d5a78cd96cd5
Timo Nolle <t.nolle@autodarts.io>
2024-01-26 02:09:04 +0100

    Update updater.sh
    Fix updater.sh to use proper version command

--- updater.sh
+++ updater.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CURRENT=$(~/.local/bin/autodarts -version)
+CURRENT=$(~/.local/bin/autodarts --version)
 LATEST=$(curl -sL https://api.github.com/repos/autodarts/releases/releases/latest | grep tag_name | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
 
 echo "Current version: ${CURRENT}, latest version: ${LATEST}"
